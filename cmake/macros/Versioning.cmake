1529705e7bb1e71a78a8b51236289575f7b2ad7f
Donny Lawrence <trea@treamous.com>
2018-05-20 16:10:24 -0500

    cmake: Remove SOVERSION

--- cmake/macros/Versioning.cmake
+++ cmake/macros/Versioning.cmake
@@ -3,7 +3,6 @@ function(add_library target_name)
   get_target_property(type "${target_name}" TYPE)
   if(type STREQUAL "SHARED_LIBRARY")
     set_target_properties("${target_name}" PROPERTIES
-      VERSION "${PROJECT_VERSION}"
-      SOVERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
+      VERSION "${PROJECT_VERSION}")
   endif()
 endfunction(add_library)
