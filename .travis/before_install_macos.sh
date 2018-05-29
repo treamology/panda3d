0bd9ce719cd13061b8b72863938a3a25aee86816
Donny Lawrence <trea@treamous.com>
2018-05-29 12:22:56 -0500

    Try removing 'brew update'

--- .travis/before_install_macos.sh
+++ .travis/before_install_macos.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-brew update;
 brew install eigen libtar fftw libsquish zlib ffmpeg freetype bullet \
 ode wxmac opencv assimp libvorbis openssl@1.0 || true;
-
-rm /usr/local/bin/virtualenv;
-${PYTHON_INTERP:-python3} -m pip install virtualenv;
-
-if [[ "$CXX" == "g++" ]]; then
-  brew link --overwrite gcc;
-fi
