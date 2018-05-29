0e474f380b285c7abd66acb957f443d64ca3c47f
Donny Lawrence <trea@treamous.com>
2018-05-29 12:53:23 -0500

    Make sure the correct python version is used.

--- .travis/before_install_macos.sh
+++ .travis/before_install_macos.sh
@@ -3,3 +3,4 @@
 brew install eigen libtar fftw libsquish zlib ffmpeg freetype bullet \
 ode wxmac opencv assimp libvorbis openssl@1.0 || true;
 ${PYTHON_INTERP:-python3} -m pip install virtualenv;
+export $MODULE_PREFIX="${PYTHON_INTERP:-python3} -m "
