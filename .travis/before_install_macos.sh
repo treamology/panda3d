d1b67bbee1e63e8739b1aa134a68d0694a5f6a1d
Donny Lawrence <trea@treamous.com>
2018-05-29 12:47:19 -0500

    Only explicitly install pip on macOS

--- .travis/before_install_macos.sh
+++ .travis/before_install_macos.sh
@@ -2,3 +2,4 @@
 
 brew install eigen libtar fftw libsquish zlib ffmpeg freetype bullet \
 ode wxmac opencv assimp libvorbis openssl@1.0 || true;
+${PYTHON_INTERP:-python3} -m pip install virtualenv;
