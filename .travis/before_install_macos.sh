b4077c918b1430b9534cc71f7509bde2e167daf5
Donny Lawrence <trea@treamous.com>
2018-05-29 13:03:11 -0500

    Remove stupid dollar sign

--- .travis/before_install_macos.sh
+++ .travis/before_install_macos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-brew install eigen libtar fftw libsquish zlib ffmpeg freetype bullet \
-ode wxmac opencv assimp libvorbis openssl@1.0 || true;
-${PYTHON_INTERP:-python3} -m pip install virtualenv;
-export $MODULE_PREFIX="${PYTHON_INTERP:-python3} -m "
+brew install python@3 eigen libtar fftw libsquish zlib ffmpeg freetype bullet \
+ode wxmac opencv assimp libvorbis openssl@1.0 || true
+
+export MODULE_PREFIX="${PYTHON_INTERP:-python3} -m"
+$MODULE_PREFIX pip install virtualenv
