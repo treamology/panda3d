8474b505d8b5f0e1770c95dfb87d23072c8c4e03
Donny Lawrence <trea@treamous.com>
2018-05-30 16:08:13 -0500

    Forgot the hashbang

--- .travis/script_macos.sh
+++ .travis/script_macos.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ "$PYTHON_INTERP" == "python2.7" ]]; then
   export PY2_CMAKE_ARGS="-DPYTHON_EXECUTABLE=/usr/local/bin/python -DPYTHON_LIBRARY=/usr/local/opt/python@2/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib"
 fi
